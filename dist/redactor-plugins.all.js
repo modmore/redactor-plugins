b853179b1d33c399436e1c2822dbe4c4009a8c70
JP DeVries <mail@devries.jp>
2016-05-08 19:22:58 +0200

    updating replace plugin

--- dist/redactor-plugins.all.js
+++ dist/redactor-plugins.all.js
@@ -169,82 +169,95 @@
 	};
 })(jQuery);
 
-if (!RedactorPlugins) var RedactorPlugins = {};
+(function($) {
+    $.Redactor.prototype.replacer = function() {
+        return {
+            getTemplate: function() {
+                return String() +
+										'<form id="redactor-modal-replacer">' +
+	                    '<section>' +
+			                    '<p>Search and replace found text.</p>' +
+													'<div>' +
+				                    '<label>' + 'Search' + '</label>' +
+				                    '<input type="text" size="5" value="" id="redactor-replacer-find" required />' +
+													'</div>' +
+													'<div>' +
+				                    '<label>' + 'Replace' + '</label>' +
+				                    '<input type="text" size="5" value="" id="redactor-replacer-replace" required />' +
+													'</div>' +
+			                    '<div class="options">' +
+				                    '<div class="ignore-case">' +
+					                    '<label>' + 'Ignore Case' + '</label>' +
+					                    '<input type="checkbox" id="redactor-replacer-ignore-case" />' +
+				                    '</div>'
+				                    //+ '<div class="replace-all">'
+				                    //	+ '<label>' + 'Replace All' + '</label>'
+				                    //	+ '<input type="checkbox" id="redactor-replacer-replace-all" checked disabled readonly />'
+				                    //+ '</div>'
+				                    +
+			                    '</div>' +
+													'<footer>'
+														+ '<button id="redactor-modal-button-action" type="submit">Replace</button>'
+														+ '<button id="redactor-modal-button-cancel">Cancel</button>'
+												+ '</footer>'
+	                    + '</section>'
+										+ '</form>';
+            },
+            init: function() {
+                var that = this;
 
-(function($)
-{
-	RedactorPlugins.replacer = function()
-	{
-		return {
-			getTemplate: function()
-			{
-				return String()
-				+ '<section id="redactor-modal-replacer">'
-                    + '<p>Search and replace found text.</p>'
-					+ '<label>' + 'Search' + '</label>'
-					+ '<input type="text" size="5" value="2" id="redactor-replacer-find" required />'
-					+ '<label>' + 'Replace' + '</label>'
-					+ '<input type="text" size="5" value="3" id="redactor-replacer-replace" required />'
-                    + '<div class="options">'
-                        + '<div class="ignore-case">'
-        					+ '<label>' + 'Ignore Case' + '</label>'
-        					+ '<input type="checkbox" id="redactor-replacer-ignore-case" />'
-                        + '</div>'
-                        //+ '<div class="replace-all">'
-        				//	+ '<label>' + 'Replace All' + '</label>'
-        				//	+ '<input type="checkbox" id="redactor-replacer-replace-all" checked disabled readonly />'
-                        //+ '</div>'
-                    + '</div>'
-				+ '</section>';
-			},
-			init: function()
-			{                
-                //var button = this.button.add('replacer', 'replacer');
-                
-                //this.button.setAwesome('replacer', 'icon icon-eye');
-                //this.button.addCallback(button, this.replacer.show);
-                                
                 $.extend(this.opts.shortcuts, {
-                    'ctrl+f': {func:'replacer.show',params:[]}
+                    'ctrl+f': {
+                        func: 'replacer.show',
+                        params: []
+                    }
                 });
-			},
-            show: function()
-            {
-				this.modal.addTemplate('replacer', this.replacer.getTemplate());
+            },
+            show: function() {
+                var that = this;
 
-				this.modal.load('replacer', this.lang.get('replacer') || 'Replacer', 300);
-				this.modal.createCancelButton();
+                this.modal.addTemplate('replacer', this.replacer.getTemplate());
 
-				var button = this.modal.createActionButton('Replace');
-				button.on('click', this.replacer.replace);
- 
-                if(this.sel && this.sel.toString()) {
+                this.modal.load('replacer', this.lang.get('replacer') || 'Replacer', 400);
+                //this.modal.createCancelButton();
+
+                //var button = this.modal.createActionButton('Replace');
+                //button.on('click', this.replacer.replace);
+
+								$('#redactor-modal-replacer').submit(function(e){
+									e.preventDefault();
+									that.replacer.replace();
+								});
+
+                if (this.sel && this.sel.toString()) {
                     $('#redactor-replacer-find').val(this.sel.toString());
                     $('#redactor-replacer-replace').focus();
                 }
-                
-				this.selection.save();
-				this.modal.show();
 
-				$('#redactor-modal-replacer').focus();
+                this.selection.save();
+                this.modal.show();
+
+                $('#redactor-modal-replacer').focus();
+
             },
             replace: function() {
                 var _f = $('#redactor-replacer-find').val();
                 var _r = $('#redactor-replacer-replace').val();
-                var _i = $('#redactor-replacer-ignore-case').is( ":checked" );
-                
-                if(_f && _r) this.insert.set(replaceAll(_f,_r,this.$editor.html(),'g' + ((_i) ? 'i' : '')));
-                
-				this.modal.close();
-				this.selection.restore();
-                
+                var _i = $('#redactor-replacer-ignore-case').is(":checked");
+
+                if (_f && _r) this.insert.set(replaceAll(_f, _r, this.code.get(), 'g' + ((_i) ? 'i' : '')));
+
+                this.modal.close();
+                this.selection.restore();
+
                 function replaceAll(find, replace, str, flags) {
-                  return str.replace((find instanceof RegExp) ? find : new RegExp(find, flags), replace);
+                    return str.replace((find instanceof RegExp) ? find : new RegExp(find, flags), replace);
                 }
             }
-		};
-	};
+        };
+    };
 })(jQuery);
+
 if (!RedactorPlugins) var RedactorPlugins = {};
 
 (function($)
