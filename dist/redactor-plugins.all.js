7ffd7f05f3bbc15dfb8329cf8e80b6adf1b17f27
JP DeVries <mail@devries.jp>
2016-05-08 15:06:43 +0200

    adding download plugin

--- dist/redactor-plugins.all.js
+++ dist/redactor-plugins.all.js
@@ -56,24 +56,22 @@
 	};
 })(jQuery);
 
-if (!RedactorPlugins) var RedactorPlugins = {};
-
 (function($)
 {
-	RedactorPlugins.download = function()
-	{
+	$.Redactor.prototype.download = function() {
 		return {
 			init: function() {
+        var that = this;
 				var button = this.button.add('download', 'Download');
-                this.button.setAwesome('download', 'icon icon-download');
 				this.button.addCallback(button, this.download.download);
 			},
-            download: function() {
-                $('<a target="_blank" download="content.html" href="' + 'data:text/html,' + encodeURI(this.code.get()) + '"></a>').get(0).click();
-            }
+			download: function() {
+					$('<a target="_blank" download="content.html" href="' + 'data:text/html,' + encodeURI(this.code.get()) + '"></a>').get(0).click();
+			}
 		};
 	};
 })(jQuery);
+
 (function($)
 {
 	$.Redactor.prototype.imagepx = function() {
