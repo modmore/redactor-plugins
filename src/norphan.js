01241af8ff551e201015762b51c002167233ff35
JP DeVries <mail@devries.jp>
2016-05-08 16:47:51 +0200

    updating norphan plugin

--- src/norphan.js
+++ src/norphan.js
@@ -1,52 +1,60 @@
-if (!RedactorPlugins) var RedactorPlugins = {};
+if(String.prototype.reverse === undefined) {
+	String.prototype.reverse = function() {
+		return this.split('').reverse().join('');
+	}
+}
+
 
 (function($)
 {
-	RedactorPlugins.norphan = function()
-	{
+	$.Redactor.prototype.norphan = function() {
 		return {
 			init: function() {
-                if(this.opts.syncBeforeCallback !== undefined || !this.opts.tabifier) return;
-                
-                var that = this;
-                
-                that.opts.syncBeforeCallback = function(html) {
-                    var _w = $(document.createElement('div'));
-                    _w.html(html);
-                    _w.find('div,p,h1,h2,h3,h4,h5,h6,blockquote,dd,dt,figcaption').each(function(){
-                        $(this).replaceWith(that.norphan.adopt($(this).get(0).outerHTML));
-                    });
+        var that = this;
 
-                    return that.tabifier.get(_w.html());
-                };
+				if(this.opts.callbacks.syncBefore !== undefined) return;
+
+				$.extend(this.opts.callbacks, {
+					syncBefore:function(html){
+						var _w = $(document.createElement('div'));
+						_w.html(html);
+						_w.find('div,p,h1,h2,h3,h4,h5,h6,blockquote,dd,dt,figcaption').each(function(){
+								$(this).replaceWith(that.norphan.adopt($(this).get(0).outerHTML));
+						});
+						return that.code.cleaned(_w.html());
+					}
+				});
 			},
-            adopt: function(html) {
-                var _t = '';
+			adopt: function(html) {
+				var that = this;
 
-                var _a = String(html).reverse().split('');
-                var _firstSpace = false;
-                var _sniffingNonWhiteSpace = false;
+				var _t = '';
 
-                for(var i = 0; i < _a.length; i++) {
-                    if(_a[i] == '>') {
-                        _ignore = true;
-                    } else if(_a[i] == '<') {
-                        _ignore = false;
-                        _sniffingNonWhiteSpace = true;
-                    } else if(_a[i].trim().length > 0) {
-                        _sniffingNonWhiteSpace = false;
-                    }
-    
-                    if(!_ignore && !_firstSpace && _a[i] == ' ' && !_sniffingNonWhiteSpace) {
-                        _t += String('&nbsp;').reverse();
-                        _firstSpace = true;
-                    } else {
-                        _t += _a[i];
-                    }
-                }
+				var _a = String(html).reverse().split('');
+				var _firstSpace = false;
+				var _sniffingNonWhiteSpace = false;
 
-                return _t.reverse();
-            }
+				for(var i = 0; i < _a.length; i++) {
+						if(_a[i] == '>') {
+								_ignore = true;
+						} else if(_a[i] == '<') {
+								_ignore = false;
+								_sniffingNonWhiteSpace = true;
+						} else if(_a[i].trim().length > 0) {
+								_sniffingNonWhiteSpace = false;
+						}
+
+						if(!_ignore && !_firstSpace && _a[i] == ' ' && !_sniffingNonWhiteSpace) {
+								_t += String('&nbsp;').reverse();
+								_firstSpace = true;
+						} else {
+								_t += _a[i];
+						}
+				}
+
+				return _t.reverse();
+
+			}
 		};
 	};
 })(jQuery);
